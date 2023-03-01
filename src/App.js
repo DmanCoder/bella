b779865f54426c372ef1152a72477e11dbf5b03e
DmanCoder <ktdenis.1@live.com>
2023-03-01 17:25:30 +1000

    Effectively using scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 
+// Routes
 import router from "./routes";
+
+// Styles
+import "./styles/main.scss";
 
 const App = () => {
   return <RouterProvider router={router} />;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,10 @@
-import { Link } from "react-router-dom";
+// Styles
+import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <Link to="/data">Data Page</Link>
-        <h2>Home Page</h2>
-        <h2>Home Page</h2>
-        <h2>Home Page</h2>
-        <h2>Home Page</h2>
-        <h2>Home Page</h2>
         <h2>Home Page</h2>
         <h2>Home Page</h2>
       </div>
