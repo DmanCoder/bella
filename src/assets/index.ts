12b7aad43efbe8855d7abb36acd7ace5944851b9
DmanCoder <ktdenis.1@live.com>
2023-03-02 12:42:22 +1000

    Added logo

---
+++ src/assets/index.ts
@@ -0,0 +1,39 @@
+import PrimaryLogo from "./logos/primary-logo.svg";
+
+const Assets = {
+  Logos: {
+    PrimaryLogo,
+  },
+  // Icons: {
+  //   ArrowDropdown,
+  //   Account,
+  //   Home,
+  //   GraphicEQ,
+  //   MoreHoriz,
+  //   Search,
+  //   Notification,
+  //   Exit,
+  //   Play,
+  //   Shuffle,
+  //   DoubleArrow,
+  //   Star,
+  //   PlusHeart,
+  //   PlusHeartFill,
+  //   PrevArrow,
+  //   NextArrow,
+  //   RemoteTv,
+  //   Movies,
+  //   People,
+  //   MyList,
+  //   Language,
+  //   TvCast,
+  // },
+  // imgs: {
+  //   AustraliaFlag,
+  //   CanadaFlag,
+  //   UnitedKingdom,
+  //   UnitedStates,
+  // },
+};
+
+export default Assets;
--- src/components/TopNavigation/TopNavigation.js
+++ src/components/TopNavigation/TopNavigation.js
@@ -7,8 +7,13 @@ import "./TopNavigation.scss";
 const TopNavigation = () => {
   return (
     <React.Fragment>
+      {/* Logo */}
+      <div className="logo">
+        <span className="logo__img"></span>
+        <span className="logo__text">Bella</span>
+      </div>
 
-      {/* Main navigation menu */}
+      {/* Main navigation with list items */}
       <nav className="nav">
         <ul className="nav__list">
           <li className="nav__list-item">
@@ -27,7 +32,7 @@ const TopNavigation = () => {
             </a>
           </li>
           <li className="nav__list-item">
-            <a href="#0" className="nav__list-anchor animate-out">
+            <a href="#0" className="nav__list-anchor">
               How We Work
             </a>
           </li>
@@ -38,6 +43,8 @@ const TopNavigation = () => {
           </li>
         </ul>
       </nav>
+
+      {/* Hamburger */}
     </React.Fragment>
   );
 };
